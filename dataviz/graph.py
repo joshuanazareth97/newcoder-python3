930e49a58fd60ff916a51cb66e30d137b8f0f536
Joshua <joshuanazareth97@gmail.com>
2018-07-27 00:05:24 +0530

    Added function plot_count_of() which saves a png file of the specified graph, based on the data parsed

---
+++ dataviz/graph.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from collections import Counter
+
+def plot_count_of(parsed_data, term, mode="plot", w=11, h=6, filename = None):
+    """Takes a list of dicts and plots (as a line graph) the number of times
+    a certain value occurs."""
+    if filename is None: filename = term
+    count =  Counter(item[term] for item in parsed_data)
+    x = [key for key in count.keys()]
+    y = [count[key] for key in x]
+    plt.figure(figsize = (w,h))
+    plt.ylabel('Count')
+    plt.xlabel(term)
+    plt.xticks(rotation=90)
+    plt.subplots_adjust(bottom=0.5)
+    if mode=="bar": plt.bar(x,y)
+    else: plt.plot(x,y)
+    plt.savefig("{}.png".format(filename))
+    plt.clf()
