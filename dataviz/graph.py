208c097fae6a76896419a02a4dd3160b93440fd4
Joshua <joshuanazareth97@gmail.com>
2018-07-27 00:32:46 +0530

    Changed code to save the generated graphs in the Images sub-directory

--- dataviz/graph.py
+++ dataviz/graph.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 def plot_count_of(parsed_data, term, mode="plot", w=11, h=6, filename = None):
-    """Takes a list of dicts and plots (as a line graph) the number of times
-    a certain value occurs."""
+    """Takes a list of dicts and plots (as a graph) the number of times
+    a certain key is present."""
     if filename is None: filename = term
+    filename = "Images/" + filename
     count =  Counter(item[term] for item in parsed_data)
     x = [key for key in count.keys()]
     y = [count[key] for key in x]
